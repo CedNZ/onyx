11c7f2fdaf4a78c5e4b303143e37fd348cd9522a
Ced <cbourneville@gmail.com>
2023-04-01 12:26:48 +1300

    Shift Fan-On logic up to DeviceInteraction page, and attach click handler to container

--- src/features/deviceInteraction/DeviceInteraction.jsx
+++ src/features/deviceInteraction/DeviceInteraction.jsx
@@ -7,8 +7,21 @@ import TargetTemperatureRange from "./TargetTemperatureRange/TargetTemperatureRa
 
 import WorkFlow from "../workflowEditor/WorkflowButtons";
 import Container from "react-bootstrap/Container";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import styled from "styled-components";
+
+import { useCallback, useEffect, useRef } from "react";
+import {
+    convertToUInt8BLE,
+    convertBLEtoUint16,
+    convertToggleCharacteristicToBool,
+} from "../../services/utils";
+import { getCharacteristic } from "../../services/BleCharacteristicCache";
+import { fanOnUuid, fanOffUuid, register1Uuid } from "../../constants/uuids";
+import { fanMask } from "../../constants/masks";
+import { setIsFanOn } from "./deviceInteractionSlice";
+import store from "../../store";
+import { AddToQueue, AddToPriorityQueue } from "../../services/bleQueueing";
 
 const Div = styled.div`
   align-self: flex-end;
@@ -18,32 +31,80 @@ const Div = styled.div`
   flex-grow: 1;
 `;
 function Volcano() {
-  /* eslint-disable no-unused-vars */
-  //little hack to make Pridetext reaminate when these states change
-  const currentTargetTemperature = useSelector(
-    (state) => state.deviceInteraction.targetTemperature
-  );
+    /* eslint-disable no-unused-vars */
+    //little hack to make Pridetext reaminate when these states change
+    const currentTargetTemperature = useSelector(
+        (state) => state.deviceInteraction.targetTemperature
+    );
 
-  const currentTemperature = useSelector(
-    (state) => state.deviceInteraction.currentTemperature
-  );
-  /* eslint-enable no-unused-vars */
+    const currentTemperature = useSelector(
+        (state) => state.deviceInteraction.currentTemperature
+    );
+    /* eslint-enable no-unused-vars */
 
-  return (
-    <Container style={{ display: "flex" }}>
-      <Div>
-        <CurrentTemperature />
-        <CurrentTargetTemperature />
-        <WriteTemperature />
-        <WorkFlow />
-        <TargetTemperatureRange />
-        <div className="heat-air-div">
-          <HeatOn />
-          <FanOn />
-        </div>
-      </Div>
-    </Container>
-  );
+    const isFanOn = useSelector((state) => state.deviceInteraction.isFanOn);
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        if (isFanOn === undefined) {
+            const blePayload = async () => {
+                const characteristicPrj1V = getCharacteristic(register1Uuid);
+                const value = await characteristicPrj1V.readValue();
+                const currentVal = convertBLEtoUint16(value);
+                const initialFanValue = convertToggleCharacteristicToBool(currentVal, fanMask);
+                if (store.getState().deviceInteraction.isFanOn !== initialFanValue) {
+                    dispatch(setIsFanOn(initialFanValue));
+                }
+            };
+            AddToQueue(blePayload);
+        }
+    }, [dispatch, isFanOn]);
+
+    const onClick = useCallback((nextState) => {
+        const blePayload = async () => {
+            const targetCharacteristicUuid = nextState ? fanOnUuid : fanOffUuid;
+            const characteristic = getCharacteristic(targetCharacteristicUuid);
+            const buffer = convertToUInt8BLE(0);
+            await characteristic.writeValue(buffer);
+        };
+        AddToPriorityQueue(blePayload);
+    }, []);
+
+    const fanOnRef = useRef(null);
+    const spaceBarKeycode = 32;
+    useEffect(() => {
+        const handler = (e) => {
+            if (e.keyCode === spaceBarKeycode) {
+                fanOnRef.current.click();
+            }
+        };
+        document.addEventListener('keyup', handler);
+
+        return () => {
+            document.removeEventListener('keyup', handler);
+        };
+    }, [onClick]);
+
+    const clickHandler = (e) => {
+        e.preventDefault();
+        onClick(!isFanOn);
+    };
+
+    return (
+        <Container style={{ display: "flex" }} onClick={clickHandler}>
+            <Div>
+                <CurrentTemperature />
+                <CurrentTargetTemperature />
+                <WriteTemperature />
+                <WorkFlow />
+                <TargetTemperatureRange />
+                <div className="heat-air-div">
+                    <HeatOn />
+                    <FanOn fanOnRef={fanOnRef} onChange={onClick} isFanOn={isFanOn} />
+                </div>
+            </Div>
+        </Container>
+    );
 }
 
 export default Volcano;
